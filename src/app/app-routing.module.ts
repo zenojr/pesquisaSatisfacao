59dcf5a1e47133bea741e2bc27eb6a36fef552c6
zenojr <zfrancajunior@gmail.com>
2019-03-13 08:18:55 -0300

    authguard OK!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './login/auth.guard';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,18 +6,13 @@ import { LoginComponent } from './login/login.component';
 import { PesquisaComponent } from './pesquisa/pesquisa.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LoginComponent
-  },
-  {
-    path: 'pesquisa',
-    component: PesquisaComponent
-  }
+  {path: '', component: LoginComponent},
+  {path: 'pesquisa', component: PesquisaComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
