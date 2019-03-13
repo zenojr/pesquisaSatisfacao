079457092528513430f4e42663528a0a2d284934
zenojr <zfrancajunior@gmail.com>
2019-03-13 08:35:01 -0300

    remove junk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
-import { PerguntasComponent } from './perguntas/perguntas.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component'
@@ -27,7 +26,6 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     LoginComponent,
     ToolbarComponent,
     PesquisaComponent,
-    PerguntasComponent,
     HeaderComponent,
     SidenavListComponent
   ],
