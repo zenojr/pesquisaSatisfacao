2fa5fb6b3c249f88a988bbd44e7bb7d5d5eaf5b7
zenojr <zfrancajunior@gmail.com>
2019-08-28 15:30:38 -0300

    added file

--- src/app/gerencial/relatorios-gchart/relatorios-gchart.component.ts
+++ src/app/gerencial/relatorios-gchart/relatorios-gchart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-relatorios-gchart',
@@ -9,39 +9,45 @@ import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces
 })
 export class RelatoriosGChartComponent implements OnInit {
 
-  public pieChart: GoogleChartInterface = {
-    chartType: 'PieChart',
-    dataTable: [
-      ['Task', 'Hours per Day'],
-      ['Work',     11],
-      ['Eat',      2],
-      ['Commute',  2],
-      ['Watch TV', 2],
-      ['Sleep',    7]
-    ],
-    // opt_firstRowIsData: true,
-    options: {'title': 'Tasks'},
-  };
+  workdata = 20;
+
+  // public pieChart: GoogleChartInterface = {
+  //   chartType: 'PieChart',
+  //   dataTable: [
+  //     ['Task', 'Hours per Day'],
+  //     ['Work',     this.workdata],
+  //     ['Eat',      2],
+  //     ['Commute',  2],
+  //     ['Watch TV', 2],
+  //     ['Sleep',    7]
+  //   ],
+  //   // opt_firstRowIsData: true,
+  //   options: {'title': 'Tasks'},
+  // };
 
   public barChart: GoogleChartInterface = {
-    chartType: 'BarChart',
+    chartType: 'ColumnChart',
     dataTable: [
-      ['Task', 'Hours per Day'],
-      ['Work',     11],
-      ['Eat',      2],
-      ['Commute',  2],
-      ['Watch TV', 2],
-      ['Sleep',    7]
+      ['Task', 'Hours per Day', {role: 'annotation'}],
+      ['Work',     11 , '20%'],
+      ['Eat',      2 , '20%'],
+      ['Commute',  2, '20%'],
+      ['Watch TV', 2, '20%'],
+      ['Sleep',    7, '20%']
     ],
     // opt_firstRowIsData: true,
     options: {'title': 'Tasks'},
   };
 
-  constructor() {
+  constructor( private db: AngularFirestore ) {
 
    }
 
   ngOnInit() {
+  }
+
+  consultaAspecTec() {
+    
   }
 
 
