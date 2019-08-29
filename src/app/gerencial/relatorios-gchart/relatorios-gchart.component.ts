1ea624ef49cfdce3027285f68749180d2b0c4f3f
zenojr <zfrancajunior@gmail.com>
2019-08-29 11:12:26 -0300

    update proj

--- src/app/gerencial/relatorios-gchart/relatorios-gchart.component.ts
+++ src/app/gerencial/relatorios-gchart/relatorios-gchart.component.ts
@@ -1,7 +1,15 @@
-
 import { Component, OnInit } from '@angular/core';
 import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RespostasChart } from './../../shared/contaResp.model';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/combineLatest';
+
+export interface Item {
+  conta: number;
+}
 
 @Component({
   selector: 'app-relatorios-gchart',
@@ -10,54 +18,63 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class RelatoriosGChartComponent implements OnInit {
 
-  // public pieChart: GoogleChartInterface = {
-  //   chartType: 'PieChart',
-  //   dataTable: [
-  //     ['Task', 'Hours per Day'],
-  //     ['Work',     this.workdata],
-  //     ['Eat',      2],
-  //     ['Commute',  2],
-  //     ['Watch TV', 2],
-  //     ['Sleep',    7]
-  //   ],
-  //   // opt_firstRowIsData: true,
-  //   options: {'title': 'Tasks'},
-  // };
+  items$: Observable<Item[]>;
+  contaFilter$: BehaviorSubject<string|null>;
 
 
   constructor( private db: AngularFirestore ) {
+    this.contaFilter$ = new BehaviorSubject(null);
+    this.items$ = Observable.combineLatest(
+      this.contaFilter$
+    ).switchMap(([conta]) =>
+      db.collection<Item>( 'Assistência Técnica', ref => {
+        let query: firebase.firestore.Query = ref;
+        if (conta) { query = query.where( 'respostaCorfio', '==', 'ótimo' ); }
+        return query;
+      } ).valueChanges()
+    );
+  }
 
-   }
+
 
    workdata = 1;
 
-    public barChart: GoogleChartInterface;
+    public barChart: GoogleChartInterface = {
+      chartType: 'ColumnChart',
+      dataTable: [
+        ['Task', 'Hours per Day', {role: 'annotation'}],
+        ['Work',     'this.contaOtimo' , 'dataPercent'],
+        ['Eat',      2 , '20%'],
+        ['Commute',  2, '20%'],
+        ['Watch TV', 2, '20%'],
+        ['Sleep',    7, '20%']
+      ],
+      // opt_firstRowIsData: true,
+      options: {'title': 'Tasks'},
+    };
 
   ngOnInit() {
     this.consultaAspecTec();
   }
 
   consultaAspecTec() {
-    const astecCorfio = this.db.collection('Assistência Técnica', ref => ref.where( 'respostaCorfio', '==', 'ótimo' ))
-                        .valueChanges().subscribe(doc => {
-                          this.workdata = doc.length;
-                          let dataPercent = this.workdata + '%' ;
-                          this.barChart = {
-                            chartType: 'ColumnChart',
-                            dataTable: [
-                              ['Task', 'Hours per Day', {role: 'annotation'}],
-                              ['Work',     this.workdata , dataPercent],
-                              ['Eat',      2 , '20%'],
-                              ['Commute',  2, '20%'],
-                              ['Watch TV', 2, '20%'],
-                              ['Sleep',    7, '20%']
-                            ],
-                            // opt_firstRowIsData: true,
-                            options: {'title': 'Tasks'},
-                          };
-                        }) ;
+    const astecCorfioOtimo = this.db.collection('Assistência Técnica',
+    ref => ref.where( 'respostaCorfio', '==', 'ótimo' )).snapshotChanges();
+    const astecCorfioBom   = this.db.collection('Assistência Técnica', ref => ref.where( 'respostaCorfio', '==', 'bom' )).valueChanges();
 
-  console.log(astecCorfio);
+
+    return this.workdata;
+
+    // this.contaOtimo = this.db.collection('Assistência Técnica', ref => ref.where( 'respostaCorfio', '==', 'ótimo' ))
+    //                   .valueChanges().subscribe(doc => console.log(doc.length));
+
+    // astecCorfioOtimo.subscribe({
+    //   next(response) { console.log(response.length); contaOtimo = response.length; },
+    //   error(err) { console.log('Error: ' + err); },
+    //   complete() { console.log('End'); }
+    // });
+    // console.log(contaOtimo);
+
 
   }
 
---
+++ src/app/shared/contaResp.model.ts
@@ -0,0 +1,4 @@
+export class RespostasChart {
+    opcao: string;
+    contador: number;
+}
