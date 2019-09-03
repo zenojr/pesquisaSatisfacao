fef39d2155323976a63852451624301fc4404353
zenojr <zfrancajunior@gmail.com>
2019-09-03 09:47:41 -0300

    component image and products OK

--- src/app/gerencial/relatorios-gchart/relatorios-img-prod/relatorios-img-prod.component.ts
+++ src/app/gerencial/relatorios-gchart/relatorios-img-prod/relatorios-img-prod.component.ts
@@ -10,7 +10,9 @@ import { AngularFirestore     } from '@angular/fire/firestore';
 })
 export class RelatoriosImgProdComponent implements OnInit {
 
-  graphAceitProd: GoogleChartInterface;
+  graphAceitProd:   GoogleChartInterface;
+  graphConceitProd: GoogleChartInterface;
+  graphProdConhec:  GoogleChartInterface;
 
   showProg    = true;
   alreadyLoad = false;
@@ -26,8 +28,146 @@ export class RelatoriosImgProdComponent implements OnInit {
 
     if (!this.alreadyLoad) {
       this.respAceitProd();
+      this.respConceitProd();
+      this.respProdConhec();
       this.alreadyLoad = true;
     }
+  }
+
+  respProdConhec() {
+    let otimoCorfio   = 0;
+    let otimoOutros   = 0;
+    let bomCorfio     = 0;
+    let bomOutros     = 0;
+    let regularCorfio = 0;
+    let regularOutros = 0;
+    let ruimCorfio    = 0;
+    let ruimOutros    = 0;
+    let naoUsoCorfio  = 0;
+    let naoUsoOutros  = 0;
+    const customQuest   = null;
+    const pergunta    = 'Os produtos são conhecidos?';
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'ótimo' ))
+                                     .valueChanges()
+                                     .subscribe( doc => otimoCorfio = doc.length );
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'ótimo' ))
+                                     .valueChanges()
+                                     .subscribe(doc => otimoOutros = doc.length );
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'bom' ))
+                                     .valueChanges()
+                                     .subscribe(doc => bomCorfio = doc.length);
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'bom' ))
+                                     .valueChanges()
+                                     .subscribe(doc => bomOutros = doc.length);
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'regular' ))
+                                     .valueChanges()
+                                     .subscribe(doc => regularCorfio = doc.length);
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'regular' ))
+                                     .valueChanges()
+                                     .subscribe(doc => regularOutros = doc.length);
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'ruim' ))
+                                     .valueChanges()
+                                     .subscribe(doc => ruimCorfio = doc.length);
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'ruim' ))
+                                     .valueChanges()
+                                     .subscribe(doc => ruimOutros = doc.length);
+
+    setTimeout(() => {
+      this.graphProdConhec = this.relService.buildGraphColumn(
+        otimoCorfio,
+        otimoOutros,
+        bomCorfio,
+        bomOutros,
+        regularCorfio,
+        regularOutros,
+        ruimCorfio,
+        ruimOutros,
+        naoUsoCorfio,
+        naoUsoOutros,
+        this.graphProdConhec,
+        pergunta,
+        customQuest);
+    }, 3000);
+  }
+
+  respConceitProd() {
+    let otimoCorfio   = 0;
+    let otimoOutros   = 0;
+    let bomCorfio     = 0;
+    let bomOutros     = 0;
+    let regularCorfio = 0;
+    let regularOutros = 0;
+    let ruimCorfio    = 0;
+    let ruimOutros    = 0;
+    let naoUsoCorfio  = 0;
+    let naoUsoOutros  = 0;
+    const customQuest   = null;
+    const pergunta    = 'Conceito do produto junto ao usuário';
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'ótimo' ))
+                                     .valueChanges()
+                                     .subscribe( doc => otimoCorfio = doc.length );
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'ótimo' ))
+                                     .valueChanges()
+                                     .subscribe(doc => otimoOutros = doc.length );
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'bom' ))
+                                     .valueChanges()
+                                     .subscribe(doc => bomCorfio = doc.length);
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'bom' ))
+                                     .valueChanges()
+                                     .subscribe(doc => bomOutros = doc.length);
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'regular' ))
+                                     .valueChanges()
+                                     .subscribe(doc => regularCorfio = doc.length);
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'regular' ))
+                                     .valueChanges()
+                                     .subscribe(doc => regularOutros = doc.length);
+
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaCorfio', '==', 'ruim' ))
+                                     .valueChanges()
+                                     .subscribe(doc => ruimCorfio = doc.length);
+    this.db.collection(pergunta, ref => ref
+                                     .where( 'respostaOutros', '==', 'ruim' ))
+                                     .valueChanges()
+                                     .subscribe(doc => ruimOutros = doc.length);
+
+    setTimeout(() => {
+      this.graphConceitProd = this.relService.buildGraphColumn(
+        otimoCorfio,
+        otimoOutros,
+        bomCorfio,
+        bomOutros,
+        regularCorfio,
+        regularOutros,
+        ruimCorfio,
+        ruimOutros,
+        naoUsoCorfio,
+        naoUsoOutros,
+        this.graphConceitProd,
+        pergunta,
+        customQuest);
+    }, 3000);
   }
 
   respAceitProd() {
